93d69b07b0f42206b0740876e0161e0f82ac57e3
nautetsu <tetsuharu.nasu@gmail.com>
2024-02-11 18:13:05 +0900

    inertia-vuejs

---
+++ resources/js/Pages/Inertia/Create.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { reactive } from 'vue';
+import { router } from '@inertiajs/vue3';
+
+const form = reactive({
+    title: null,
+    content: null,
+})
+
+const submitFunction = () => {
+    router.post('/inertia', form)
+}
+</script>
+
+<template>
+    <form @submit.prevent="submitFunction">
+        <input type="text" name="title" v-model="form.title"><br>
+        <input type="text" name="content" v-model="form.content"><br>
+        <button>送信</button>
+    </form>
+</template>
---
+++ resources/js/Pages/Inertia/Show.vue
@@ -0,0 +1,21 @@
+<script setup>
+//import { Inertia } from '@inertiajs/vue3'
+import { router } from '@inertiajs/vue3';
+
+defineProps({
+    id: String,
+    blog: Object
+})
+const deleteConfirm = id => { 
+  // JSのテンプレート構文  バックスラッシュで囲む 変数箇所を${}で囲む 
+    router.delete(`/inertia/${id}`, { 
+        onBefore: () => confirm('本当に削除しますか?') 
+    })
+}
+
+</script>
+
+<template>
+    {{ id }}<br> {{ blog.title }}<br>
+    <button @click="deleteConfirm(blog.id)">削除</button>
+</template>
